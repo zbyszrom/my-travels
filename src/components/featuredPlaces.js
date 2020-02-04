6979113f5f8af2a0bc9dd9898c4af38f6a4d1277
Zbigniew Downar <zbyszrom@Zbigniews-iMac.local>
2020-02-04 21:59:16 +0100

    featured places

--- src/components/featuredPlaces.js
+++ src/components/featuredPlaces.js
@@ -3,11 +3,42 @@ import { useStaticQuery, graphql } from "gatsby"
 import { Link } from "gatsby"
 import Place from "./place"
 
+const getPlaces = graphql`
+query {
+  featuredPlaces: allDatoCmsPlace(filter: { featured: { eq: true } }) {
+    edges {
+      node {
+        images {
+          fluid {
+            ...GatsbyDatoCmsFluid
+          }
+        }
+        name
+        slug
+        country
+       id          
+      }
+    }
+  }
+}
+`
+
+
  const FeaturedPlaces = () => {
+  const response = useStaticQuery(getPlaces)
+  const places = response.featuredPlaces.edges
     return (
-        <div>
-          <h2>Featured places are there</h2>  
-        </div>
+        <section>
+          <h4>Featured places</h4>
+          {places.map(({ node }) => {
+            return <Place  key={node.id} place = {node}/>
+          }
+          )}
+          <Link to = "/places">
+            All places
+          </Link>
+
+        </section>
     )
 }
 export default FeaturedPlaces
